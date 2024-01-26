14c06e112ac58881514f8658f5541b1c72f1a535
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-01-26 00:49:22 -0800

    Incorporate oh-my-zsh git aliases into git config

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,11 +1,18 @@
-{ pkgs, lib, ... }:
+{ lib, ... }:
 
 {
   config.programs = {
     zsh = {
       enable = true;
       enableAutosuggestions = true;
-      initExtra = builtins.readFile ./zshrc;
+      initExtra = lib.strings.concatMapStringsSep "\n\n" builtins.readFile [
+        ./zshrc
+        (builtins.fetchurl {
+          url =
+            "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/8be4789bbbef06fe5eed581dc8c58df51e3cd9fd/plugins/git/git.plugin.zsh";
+          sha256 = "1cy65hwk028wz5896157lr5m3vgb9l4b61bzkykz3sb9jmcdc930";
+        })
+      ];
       shellAliases = { ls = "ls --color"; };
     };
 
