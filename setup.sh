7ac3d271e11994cd078afbf634ae1c2778aac994
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-01-26 00:49:09 -0800

    Add nixpkgs to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ set -euxfo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/NixOS/nixpkgs/archive/master.tar.gz nixpkgs
 nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 
 nix-channel --update
 
