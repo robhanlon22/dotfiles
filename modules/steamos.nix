523333822ee082eafec84643714790861478c704
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-01-26 00:49:10 -0800

    Add to-game-mode alias

--- modules/steamos.nix
+++ modules/steamos.nix
@@ -17,6 +17,11 @@ in
             pkgs.writeScriptBin "${bin}" (builtins.readFile "${bins}/${bin}")
           )
           (builtins.attrNames (builtins.readDir bins));
+
+      programs.zsh.shellAliases = {
+        to-game-mode = "exec qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout";
+        tgm = "to-game-mode";
+      };
     }
   );
 }
