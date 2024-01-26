671587d3d622ff7f2696197be1ef4230d4aa74dc
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-01-26 00:49:13 -0800

    nix-channel can't be one command

--- setup.sh
+++ setup.sh
@@ -6,10 +6,9 @@ if ! /nix/nix-installer self-test; then
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 fi
 
-nix-channel \
-  --add https://github.com/NixOS/nixpkgs/archive/master.tar.gz nixpkgs \
-  --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl \
-  --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager \
-  --update
+nix-channel --add https://github.com/NixOS/nixpkgs/archive/master.tar.gz nixpkgs
+nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
 
 nix-shell '<home-manager>' -A install
