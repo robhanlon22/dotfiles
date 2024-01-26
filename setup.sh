63a532c7916f4bf6927cc4943ed65026c6708179
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-01-26 00:49:09 -0800

    Fixup nix-channel

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ set -euxfo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-nix-channel --add https://github.com/NixOS/nixpkgs/archive/master.tar.gz nixpkgs
-nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-
-nix-channel --update
+nix-channel \
+  --add https://github.com/NixOS/nixpkgs/archive/master.tar.gz nixpkgs \
+  --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl \
+  --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager \
+  --update
 
 nix-shell '<home-manager>' -A install
