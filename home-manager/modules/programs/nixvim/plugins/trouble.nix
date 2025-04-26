78ec2b86d26045f8909e90c93bbbe79932bd5337
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2025-04-26 00:37:07 -0700

    Fix Trouble

--- home-manager/modules/programs/nixvim/plugins/trouble.nix
+++ home-manager/modules/programs/nixvim/plugins/trouble.nix
@@ -4,13 +4,12 @@
   };
 
   my.programs.nixvim.plugins.which-key.register = with my.nixvim.keymap; let
-    troubleToggle = s: wk.vim "TroubleToggle ${s}";
+    troubleToggle = s: wk.vim "Trouble ${s} toggle";
   in [
     {
       opts.prefix = "<leader>";
       mappings = {
         t = wk.group "Trouble" {
-          t = troubleToggle "" "Toggle";
           w = troubleToggle "lsp_workspace_diagnostics" "LSP workspace diagnostics";
           d = troubleToggle "lsp_document_diagnostics" "LSP document diagnostics";
           q = troubleToggle "quickfix" "Quickfix list";
